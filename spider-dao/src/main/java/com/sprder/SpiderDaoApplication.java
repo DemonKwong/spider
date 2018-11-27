9ea8ae3373eee74a9fae2b06fe6321ad516b06ad
kuangjiewen <kjwxfn@qq.com>
2018-11-27 15:26:49 +0800

    DAO模块

---
+++ spider-dao/src/main/java/com/sprder/SpiderDaoApplication.java
@@ -0,0 +1,14 @@
+package com.sprder;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kuangjiewen
+ * @Title: SpiderDaoApplication
+ * @ProjectName spider
+ * @Description: TODO
+ * @date 2018/11/2715:21
+ */
+@SpringBootApplication
+public class SpiderDaoApplication {
+}
