6b6755e25e4db5b1ca6fe7357c41385e8bab90aa
DamonKuang <kuangjiewen1995@163.com>
2018-12-09 18:22:40 +0800

    redis配置&多环境切换配置

--- spider-core/src/main/java/com/spider/core/processor/KuaiDaiLiProcessor.java
+++ spider-core/src/main/java/com/spider/core/processor/KuaiDaiLiProcessor.java
@@ -1,6 +1,7 @@
 package com.spider.core.processor;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -10,6 +11,9 @@ import java.util.Random;
 
 @Component
 public class KuaiDaiLiProcessor implements PageProcessor {
+
+    private Logger logger = Logger.getLogger(PageProcessor.class);
+
     private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
     @Override
     public void process(Page page) {
@@ -21,8 +25,11 @@ public class KuaiDaiLiProcessor implements PageProcessor {
         page.putField("thread",thread);
         //爬不到就继续爬
         if(StringUtils.isBlank(ip) || StringUtils.isBlank(port)){
+            logger.info("ip地址或者port端口为空，将会继续爬");
             page.addTargetRequest(page.getRequest());
+            page.setSkip(true);
         }else {
+            logger.info("爬取到了");
             page.putField("ip", ip);
             page.putField("port", Integer.valueOf(port));
         }
