04cef0389d3b6efe60c185cfc08178aac00f0d8e
kuangjiewen <kjwxfn@qq.com>
2018-12-20 17:00:40 +0800

    spider-web项目

---
+++ spider-web/src/main/java/com/spider/web/SpiderWebApplication.java
@@ -0,0 +1,18 @@
+package com.spider.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kuangjiewen
+ * @Title: SpiderWebApplication
+ * @ProjectName spider
+ * @Description: 爬虫web入口
+ * @date 2018/12/20 14:46
+ */
+@SpringBootApplication
+public class SpiderWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderWebApplication.class,args);
+	}
+}
---
+++ spider-web/src/main/java/com/spider/web/controller/HelloController.java
@@ -0,0 +1,39 @@
+package com.spider.web.controller;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author kuangjiewen
+ * @Title: HelloController
+ * @ProjectName spider
+ * @Description: TODO
+ * @date 2018/12/2014:48
+ */
+@RestController
+public class HelloController {
+
+	private Logger logger = Logger.getLogger(HelloController.class);
+
+	@RequestMapping("hello")
+	public String hello(@RequestParam Map map){
+//		// 微信加密签名
+//		String signature = request.getParameter("signature");
+//		// 随机字符串
+//		String echostr = request.getParameter("echostr");
+//		// 时间戳
+//		String timestamp = request.getParameter("timestamp");
+//		// 随机数
+//		String nonce = request.getParameter("nonce");
+		JSONObject jsonObject = (JSONObject) JSONObject.toJSON(map);
+		return jsonObject.getString("echostr");
+	}
+}
