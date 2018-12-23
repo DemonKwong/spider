d544b8e31eb3029bdf55d317be9e099e84896c12
DamonKuang <kuangjiewen1995@163.com>
2018-12-23 22:45:47 +0800

    微信公众号消息接送学习

--- spider-web/src/main/java/com/spider/web/controller/HelloController.java
+++ spider-web/src/main/java/com/spider/web/controller/HelloController.java
@@ -1,14 +1,11 @@
 package com.spider.web.controller;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Map;
 
 /**
@@ -24,7 +21,7 @@ public class HelloController {
 	private Logger logger = Logger.getLogger(HelloController.class);
 
 	@RequestMapping("hello")
-	public String hello(@RequestParam Map map){
+	public String hello(@RequestParam Map map,@RequestBody WeChatMsg msg){
 //		// 微信加密签名
 //		String signature = request.getParameter("signature");
 //		// 随机字符串
@@ -34,13 +31,90 @@ public class HelloController {
 //		// 随机数
 //		String nonce = request.getParameter("nonce");
 		JSONObject jsonObject = (JSONObject) JSONObject.toJSON(map);
+		logger.info("jsonObject:"+jsonObject.toJSONString());
+		logger.info("msg:"+msg.toString());
 		return jsonObject.getString("echostr");
 	}
 
-	@RequestMapping("get")
-	public String get(@RequestParam Map map){
-		JSONObject jsonObject = (JSONObject) JSONObject.toJSON(map);
-		logger.info(jsonObject.toJSONString());
+	@RequestMapping(value = "get",method = RequestMethod.POST)
+	public String get(@RequestBody WeChatMsg msg){
+		logger.info(msg.toString());
 		return "hello";
+	}
+
+	@XmlRootElement(name = "xml")
+	static class WeChatMsg{
+		private String toUserName;
+		private String fromUserName;
+		private String createTime;
+		private String msgType;
+		private String content;
+		private String msgId;
+
+		public String getToUserName() {
+			return toUserName;
+		}
+
+		@XmlElement(name = "ToUserName")
+		public void setToUserName(String toUserName) {
+			this.toUserName = toUserName;
+		}
+
+		public String getFromUserName() {
+			return fromUserName;
+		}
+
+		@XmlElement(name = "FromUserName")
+		public void setFromUserName(String fromUserName) {
+			this.fromUserName = fromUserName;
+		}
+
+		public String getCreateTime() {
+			return createTime;
+		}
+
+		@XmlElement(name = "CreateTime")
+		public void setCreateTime(String createTime) {
+			this.createTime = createTime;
+		}
+
+		public String getMsgType() {
+			return msgType;
+		}
+
+		@XmlElement(name = "MsgType")
+		public void setMsgType(String msgType) {
+			this.msgType = msgType;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		@XmlElement(name = "Content")
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public String getMsgId() {
+			return msgId;
+		}
+
+		@XmlElement(name = "MsgId")
+		public void setMsgId(String msgId) {
+			this.msgId = msgId;
+		}
+
+		@Override
+		public String toString() {
+			return "WeChatMsg{" +
+					"toUserName='" + toUserName + '\'' +
+					", fromUserName='" + fromUserName + '\'' +
+					", createTime='" + createTime + '\'' +
+					", msgType='" + msgType + '\'' +
+					", content='" + content + '\'' +
+					", msgId='" + msgId + '\'' +
+					'}';
+		}
 	}
 }
