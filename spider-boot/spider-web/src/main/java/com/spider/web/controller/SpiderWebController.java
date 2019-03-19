8e2fe3b42f36f39372bacfef81b0ca599a1c9ced
Demon <asd5616263>
2019-03-19 22:32:26 +0800

    web调整

--- spider-boot/spider-web/src/main/java/com/spider/web/controller/SpiderWebController.java
+++ spider-boot/spider-web/src/main/java/com/spider/web/controller/SpiderWebController.java
@@ -3,21 +3,29 @@ package com.spider.web.controller;
 import com.spider.web.service.HouseInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
 
-@RestController
+@Controller
 public class SpiderWebController {
     @Autowired
     private HouseInfoService houseInfoService;
 
     @RequestMapping("/getList")
+    @ResponseBody
     public Page getList(@RequestParam int page, HttpServletResponse httpServletResponse){
-        httpServletResponse.setHeader("Access-Control-Allow-Origin","*");
         return houseInfoService.getList(page,7);
+    }
+
+    @RequestMapping("/list")
+    public String listPage(Model model){
+        model.addAttribute("ctx","http://localhost:8888/web");
+        return "lightlog-index";
     }
 }
